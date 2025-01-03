e00fbf4757c2d9c088a92c9956667e94230a28f7
Ryan McKeever <rkfmckee@gmail.com>
2025-01-03 20:32:48 +0000

    Change layout for mobile

--- client/src/pages/spellList/SpellList.tsx
+++ client/src/pages/spellList/SpellList.tsx
@@ -1,20 +1,21 @@
+import { Laptop, PhoneAndroid } from "@mui/icons-material";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import Typography from "@mui/material/Typography";
 import axios from "axios";
 import { MouseEvent, useEffect, useState } from "react";
+import { IsMobile } from "../../helpers/MuiHelpers";
 import { ReadResponse } from "../../schemas/ReadResponseSchema";
 import { Spell } from "../../schemas/spell/SpellSchema";
-import SpellTable from "./table/SpellTable";
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import ToggleButton from "@mui/material/ToggleButton";
-import { Laptop, PhoneAndroid } from "@mui/icons-material";
 import SpellCards from "./cards/SpellCards";
+import SpellTable from "./table/SpellTable";
 
 export default function SpellList() {
-    const [page, setPage] = useState<number>(5);
+    const [page, setPage] = useState<number>(0);
     const [rowsPerPage, setRowsPerPage] = useState<number>(10);
     const [totalRecords, setTotalRecords] = useState(0);
     const [spells, setSpells] = useState<Spell[]>();
-    const [viewMode, setViewMode] = useState("table");
+    const [viewMode, setViewMode] = useState<ViewMode>("table");
 
     type ViewMode = "table" | "card";
 
@@ -41,8 +42,8 @@ export default function SpellList() {
         }
     }
 
-    function showTableOrCardView() {
-        if (viewMode == "card") {
+    function showTableOrCardView(isMobile: boolean) {
+        if (viewMode == "card" || isMobile) {
             return <SpellCards spells={spells} />;
         }
 
@@ -58,30 +59,34 @@ export default function SpellList() {
         );
     }
 
+    const isMobile = IsMobile();
+
     return (
         <>
             <Typography variant="h1" gutterBottom>
                 Spells
             </Typography>
 
-            <ToggleButtonGroup
-                className="mb-4"
-                value={viewMode}
-                onChange={handleViewModeChange}
-                exclusive
-                aria-label="View mode"
-            >
-                <ToggleButton value="table" aria-label="left aligned">
-                    <Laptop className="spell-view-icon" />
-                    Table view
-                </ToggleButton>
-                <ToggleButton value="card" aria-label="centered">
-                    <PhoneAndroid className="spell-view-icon" />
-                    Card view
-                </ToggleButton>
-            </ToggleButtonGroup>
+            {!isMobile && (
+                <ToggleButtonGroup
+                    className="mb-4"
+                    value={viewMode}
+                    onChange={handleViewModeChange}
+                    exclusive
+                    aria-label="View mode"
+                >
+                    <ToggleButton value="table" aria-label="left aligned">
+                        <Laptop className="spell-view-icon" />
+                        Table view
+                    </ToggleButton>
+                    <ToggleButton value="card" aria-label="centered">
+                        <PhoneAndroid className="spell-view-icon" />
+                        Card view
+                    </ToggleButton>
+                </ToggleButtonGroup>
+            )}
 
-            {showTableOrCardView()}
+            {showTableOrCardView(isMobile)}
         </>
     );
 }
--- client/src/pages/spellList/cards/SpellCards.tsx
+++ client/src/pages/spellList/cards/SpellCards.tsx
@@ -4,27 +4,46 @@ import Typography from "@mui/material/Typography";
 import { Spell } from "../../../schemas/spell/SpellSchema";
 import SpellCard from "./SpellCard";
 import Masonry from "@mui/lab/Masonry";
+import { IsMobile } from "../../../helpers/MuiHelpers";
 
 interface SpellCardsProps {
     spells: Spell[] | undefined;
 }
 
 export default function SpellCards(props: SpellCardsProps) {
-    return props.spells && props.spells.length > 0 ? (
-        <Masonry columns={3} spacing={2}>
-            {props.spells.map((spell) => (
-                <SpellCard spell={spell} />
-            ))}
-        </Masonry>
-    ) : (
-        <Card>
-            <CardContent>
-                <div className="mb-3">
-                    <Typography variant="h5" component="div">
-                        No spells available
-                    </Typography>
-                </div>
-            </CardContent>
-        </Card>
-    );
+    function CardsLayout() {
+        const isMobile = IsMobile();
+
+        if (props.spells && props.spells.length > 0) {
+            if (isMobile) {
+                return props.spells.map((spell) => (
+                    <div key={spell.id} className="mb-4">
+                        <SpellCard spell={spell} />
+                    </div>
+                ));
+            } else {
+                return (
+                    <Masonry columns={3} spacing={2}>
+                        {props.spells.map((spell) => (
+                            <SpellCard key={spell.id} spell={spell} />
+                        ))}
+                    </Masonry>
+                );
+            }
+        } else {
+            return (
+                <Card>
+                    <CardContent>
+                        <div className="mb-3">
+                            <Typography variant="h5" component="div">
+                                No spells available
+                            </Typography>
+                        </div>
+                    </CardContent>
+                </Card>
+            );
+        }
+    }
+
+    return CardsLayout();
 }
