41b9b3bd588bc29b9d1a056dc5158d2be82a0f53
Ryan McKeever <rkfmckee@gmail.com>
2025-02-15 19:16:01 +0000

    Use Keycloak for authentication and add Swagger

---
+++ server/src/FST.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FST.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController(IHttpContextAccessor httpContextAccessor) : ControllerBase
+{
+    [HttpGet("Me")]
+    [Authorize]
+    public ActionResult GetClaims()
+    {
+        var claimsPrincipal = httpContextAccessor.HttpContext?.User;
+        return Ok(claimsPrincipal?.Claims.ToDictionary(c => c.Type, c => c.Value));
+    }
+}
---
+++ server/src/FST.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace FST.API.Extensions;
+
+internal static class ServiceCollectionExtensions
+{
+    internal static IServiceCollection AddSwaggerGenWithAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.CustomSchemaIds(id => id.FullName!.Replace('+', '-'));
+            options.AddSecurityDefinition("Keycloak", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows
+                {
+                    Implicit = new OpenApiOAuthFlow
+                    {
+                        AuthorizationUrl = new Uri(configuration["Keycloak:AuthorizationUrl"] ?? throw new Exception("Keycloak Auth Url not configured")),
+                        Scopes = new Dictionary<string, string>
+                        {
+                            { "openid", "openid" },
+                            { "profile", "profile" }
+                        }
+                    }
+                }
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Keycloak",
+                            Type = ReferenceType.SecurityScheme
+                        },
+                        In = ParameterLocation.Header,
+                        Name = "Bearer",
+                        Scheme = "Bearer"
+                    },
+                    []
+                }
+            });
+        });
+
+        return services;
+    }
+}
