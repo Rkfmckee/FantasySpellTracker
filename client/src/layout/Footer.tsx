7f464faeb425e53c2e3df4568e522b564e381b23
Ryan McKeever <rkfmckee@gmail.com>
2025-02-23 13:08:38 +0000

    Keep footer at bottom

--- client/src/layout/Footer.tsx
+++ client/src/layout/Footer.tsx
@@ -2,7 +2,7 @@ import Paper from "@mui/material/Paper";
 
 export default function Footer() {
     return (
-        <Paper className="footer" elevation={5}>
+        <Paper className="footer mt-auto" elevation={5}>
             <p>© 2025 Fantasy Spell Tracker</p>
         </Paper>
     );
